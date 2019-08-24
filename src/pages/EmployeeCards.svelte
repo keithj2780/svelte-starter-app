597112145ecf2cefd41bff256d9c520c62802263
keithj2780@gmail.com <keithj2780@gmail.com>
2019-08-24 20:06:42 +1000

    Added Sidemenu demo to EmployeeCards Demo

--- src/pages/EmployeeCards.svelte
+++ src/pages/EmployeeCards.svelte
@@ -40,7 +40,7 @@
         {/each}
     </Container>
 
-    
+
     <SideMenu side="right" header="More" top="100px">
         <a class="sidemenuitem" href="/testimonials">Testimonials</a>
         <a class="sidemenuitem" href="/cards">
@@ -59,7 +59,11 @@
     		<path fill="#000000" d="M9,2A7,7 0 0,1 16,9C16,10.57 15.5,12 14.61,13.19L15.41,14H16L22,20L20,22L14,16V15.41L13.19,14.61C12,15.5 10.57,16 9,16A7,7 0 0,1 2,9A7,7 0 0,1 9,2M5,8V10H13V8H5Z" />
 			</svg>
 		</a>
-    </SideMenu>
+		<a class="sidemenuitem" href="/">
+			<svg style="width:16px;height:16px" viewBox="0 0 24 24">
+				<path fill="#000000" d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" />
+			</svg> Info
+		</a>    </SideMenu>
 
 <script>
     import { onMount } from 'svelte';
